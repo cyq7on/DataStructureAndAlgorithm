ef86caba0e0874338d02284a1dc298411a7b6597
cyq7on <cyq7on@gmail.com>
2020-01-10 13:18:03 +0800

    Min Cost Climbing Stairs 优化到最佳

--- Project/Practice/src/com/cyq7on/leetcode/dp/BestTimeToBuyAndSellStock.java
+++ Project/Practice/src/com/cyq7on/leetcode/dp/BestTimeToBuyAndSellStock.java
@@ -3,23 +3,23 @@ package com.cyq7on.leetcode.dp;
 
 /**
 * @description
- * ay you have an array for which the ith element is the price of a given stock on day i.
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
  *
- * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
  *
- * Note that you cannot sell a stock before you buy one.
+ * 注意你不能在买入股票前卖出股票。
  *
- * Example 1:
+ * 示例 1:
  *
- * Input: [7,1,5,3,6,4]
- * Output: 5
- * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
- *              Not 7-1 = 6, as selling price needs to be larger than buying price.
- * Example 2:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
  *
- * Input: [7,6,4,3,1]
- * Output: 0
- * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
@@ -33,27 +33,40 @@ public class BestTimeToBuyAndSellStock {
         if (length < 2) {
             return 0;
         }
-        int[][] dp = new int[length][length];
+        //买入最小值
+        int minBuy = prices[0];
+        int max = 0;
         for (int i = 1; i < prices.length; i++) {
-            dp[0][i] = prices[i] - prices[0];
+            if (minBuy < prices[i]) {
+                max = Math.max(max, prices[i] - minBuy);
+            }else {
+                minBuy = prices[i];
+            }
+//            max = Math.max(max, prices[i] - minBuy);
+//            minBuy = Math.min(minBuy, prices[i]);
         }
-        int max = -1;
-        for (int i = 0; i < prices.length - 1; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                dp[i][j] = Math.max(prices[j] - prices[i],dp[i][j - 1]);
-                if (dp[i][j] > max) {
-                    max = dp[i][j];
-                }
+        return max;
+    }
+
+    public int maxProfit1(int[] prices) {
+        int length = prices.length;
+        if (length < 2) {
+            return 0;
+        }
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                max = Math.max(max, prices[j] - prices[i]);
             }
         }
 
-        return max > 0 ? max : 0;
+        return max;
     }
 
     public static void main(String[] args) {
         BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
-//        System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{7,1,5,3,6,4}));
-//        System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{7,6,4,3,1}));
+        System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{7,6,4,3,1}));
         System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{1,2}));
     }
 }
