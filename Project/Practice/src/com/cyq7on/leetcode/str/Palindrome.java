573f84e7b03c24c76eeeb61a9e3552eadfe917e3
cyq7on <cyq7on@qq.com>
2020-06-01 14:31:35 +0800

    kidsWithCandies

---
+++ Project/Practice/src/com/cyq7on/leetcode/str/Palindrome.java
@@ -0,0 +1,31 @@
+package com.cyq7on.leetcode.str;
+
+/**
+* @description
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* @author cyq7on
+* @create 2020/5/21
+**/
+public class Palindrome {
+    public String longestPalindrome(String s) {
+        if (s == null || "".equals(s)) {
+            return "";
+        }
+
+        return null;
+    }
+}
