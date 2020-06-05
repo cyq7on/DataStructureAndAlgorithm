b849d95aa42350cb3a1ec966599bb0c95cc210c9
cyq7on <cyq7on@qq.com>
2020-06-05 16:07:02 +0800

    顺时针打印矩阵

--- Project/Practice/src/com/cyq7on/sword2offer/array/SpiralMatrix.java
+++ Project/Practice/src/com/cyq7on/sword2offer/array/SpiralMatrix.java
@@ -36,10 +36,47 @@ public class SpiralMatrix {
      * 核心思想就是确定矩阵的四个顶点，然后顺时针进行添加。需要注意的有2点：
      * 1. 不要重复添加四个顶点的元素
      * 2. 如果只有一行或者一列元素，不要再进入for循环进行添加
+     *
      * @param matrix
      * @return
      */
     public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new ArrayList<>(0);
+        }
+        int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+        List<Integer> list = new ArrayList<>(matrix.length * matrix[0].length);
+        while (true) {
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            if (++top > bottom) {
+                break;
+            }
+            for (int i = top; i <= bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+            if (--right < left) {
+                break;
+            }
+            for (int i = right; i >= left; i--) {
+                list.add(matrix[bottom][i]);
+            }
+            if (--bottom < top) {
+                break;
+            }
+
+            for (int i = bottom; i > left; i--) {
+                list.add(matrix[i][left]);
+            }
+            if (++left > right) {
+                break;
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> spiralOrder1(int[][] matrix) {
         int cols = matrix.length;
         if (cols == 0) {
             return new ArrayList<>(0);
@@ -86,7 +123,9 @@ public class SpiralMatrix {
     public static void main(String[] args) {
         SpiralMatrix spiralMatrix = new SpiralMatrix();
         int[][] matrix = new int[][]{
-                {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
         System.out.println(spiralMatrix.spiralOrder(matrix));
     }
 }
