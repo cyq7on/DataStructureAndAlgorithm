18d5a990593aaa878ee03f94ea55119e3232356f
cyq7on <cyq7on@gmail.com>
2020-06-19 10:30:26 +0800

    最长回文字符串

--- Project/Practice/src/com/cyq7on/leetcode/str/Palindrome.java
+++ Project/Practice/src/com/cyq7on/leetcode/str/Palindrome.java
@@ -21,33 +21,60 @@ package com.cyq7on.leetcode.str;
  **/
 
 /**
- * @description
- * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
- *
+ * @author cyq7on
+ * @description 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <p>
  * 说明：本题中，我们将空字符串定义为有效的回文串。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: "A man, a plan, a canal: Panama"
  * 输出: true
  * 示例 2:
- *
+ * <p>
  * 输入: "race a car"
  * 输出: false
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/valid-palindrome
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- * @author cyq7on
  * @create 2020/6/19
  **/
 public class Palindrome {
     public String longestPalindrome(String s) {
-        if (s == null || "".equals(s)) {
+        if (s == null) {
             return "";
         }
 
-        return null;
+        int length = s.length();
+        if (length < 2) {
+            return s;
+        }
+
+        boolean[][] dp = new boolean[length][length];
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int maxLen = 1;
+        for (int j = 1; j < length; j++) {
+            for (int i = 0; i < j; i++) {
+                if (chars[i] == chars[j]) {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    }else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }else {
+                    dp[i][j] = false;
+                }
+
+                int len = j - i + 1;
+                if (dp[i][j] && len > maxLen) {
+                    maxLen = len;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start,start + maxLen);
     }
 
     public boolean isPalindrome(String s) {
@@ -66,7 +93,7 @@ public class Palindrome {
                 left++;
             }
 
-            while (left < right  && !Character.isLetterOrDigit(chars[right])) {
+            while (left < right && !Character.isLetterOrDigit(chars[right])) {
                 right--;
             }
 
@@ -77,7 +104,7 @@ public class Palindrome {
             if (Character.toUpperCase(chars[left]) == Character.toUpperCase(chars[right])) {
                 left++;
                 right--;
-            }else {
+            } else {
                 return false;
             }
         }
